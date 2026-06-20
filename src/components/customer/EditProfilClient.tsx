e1de6d9f1f1f3537e60b576dc00e4aa3fb642f6c
frdalf <kapas.alkohol17@gmail.com>
2026-06-21 00:36:20 +0700

    tambahin logo di petani dan admin

--- src/components/customer/EditProfilClient.tsx
+++ src/components/customer/EditProfilClient.tsx
@@ -3,6 +3,7 @@
 import { useState, useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
+import Image from 'next/image'
 import { ArrowRightOnRectangleIcon, BellIcon, ShoppingCartIcon, CameraIcon } from '@heroicons/react/24/outline'
 import Button from '@/components/ui/Button'
 import Input from '@/components/ui/Input'
@@ -102,9 +103,7 @@ export default function EditProfilClient({ user, returnPath, petaniProfile }: Ed
       <header className="bg-white border-b border-[#E7E8EC] sticky top-0 z-50">
         <div className="max-w-4xl mx-auto px-4 h-14 flex items-center justify-between">
           <Link href={returnPath || '/'} className="flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-[#006E2F]">
-              <path fillRule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z" clipRule="evenodd" />
-            </svg>
+            <Image src="/logo.png" alt="Toko Tani Logo" width={28} height={28} className="object-contain" />
             <span className="text-lg font-extrabold text-[#006E2F]">Toko Tani</span>
           </Link>
           <div className="flex items-center gap-3">
