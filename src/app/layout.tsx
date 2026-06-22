365670a70f2e972ff2af83eacaedd1ffbbbc0fe1
frdalf <kapas.alkohol17@gmail.com>
2026-06-22 14:01:44 +0700

    coba production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         {children}
         <script 
           type="text/javascript"
-          src="https://app.sandbox.midtrans.com/snap/snap.js"
+          src={process.env.MIDTRANS_IS_PRODUCTION === 'true' ? "https://app.midtrans.com/snap/snap.js" : "https://app.sandbox.midtrans.com/snap/snap.js"}
           data-client-key={process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY}
           async
         ></script>
